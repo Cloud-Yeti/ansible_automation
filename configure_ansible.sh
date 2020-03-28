393833d0c9b777cc15b0bef7171f2e111491457d
Robin Gautam <38055190+rbngtm1@users.noreply.github.com>
2020-03-28 15:33:28 -0500

    Update configure_ansible.sh

--- configure_ansible.sh
+++ configure_ansible.sh
@@ -7,3 +7,15 @@ sudo amazon-linux-extras install ansible2 -y
 sudo yum install ansible -y
 sudo yum install -y git python python-devel python-pip openssl ansible
 sudo pip install boto
+
+
+#========================================================================#
+# Below example shows how secrets manger can be used to copy ssh-keys #
+# The Secrets Manger access should be provided both for control node and managed node #
+# add below command on Condrol Node #
+cd ~/ && ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''
+cat ~/.ssh/id_rsa.pub | sed 's/^/{"/;s/$/"}/' | sed 's/ssh-rsa/pub_key\":\ "ssh-rsa/g' > ~/my_creds.json
+cretecred=`aws secretsmanager create-secret --name MyAnsibleSecretCred --description "My public key of ansible master" --secret-string file://my_creds.json --region us-east-1`
+
+# add below command on Managed Nodes #
+passkeytonode=`aws secretsmanager get-secret-value --secret-id MyAnsibleSecretCred --region us-east-1 | jq ".SecretString" | sed 's/\\n //g; s/\ \n//g; s/\\"/"/g; s/"\\n/"/g; s/^"//; s/"$//' | jq ".pub_key" | sed 's/\"//g' >> ~/.ssh/authorized_keys`
