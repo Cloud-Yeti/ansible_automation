17037dcf3d09b9e8faf1dc96c213d392227b8d7b
Robin Gautam <38055190+rbngtm1@users.noreply.github.com>
2020-03-29 15:52:15 -0500

    Update configure_ansible.sh

--- configure_ansible.sh
+++ configure_ansible.sh
@@ -15,8 +15,8 @@ sudo pip install boto
 # add below command on Condrol Node #
 # cd ~/ && ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''
 # cat ~/.ssh/id_rsa.pub | sed 's/^/{"/;s/$/"}/' | sed 's/ssh-rsa/pub_key\":\ "ssh-rsa/g' > ~/my_creds.json
-# cretecred=`aws secretsmanager create-secret --name MyAnsibleSecretCred --description "My public key of ansible master" --secret-string file://my_creds.json --region us-east-1`
+# aws secretsmanager create-secret --name MyAnsibleSecretCred --description "My public key of ansible master" --secret-string file://my_creds.json --region us-east-1
 #========================================================================#
 # add below command on Managed Nodes #
 # sudo yum install jq -y
-# passkeytonode=`aws secretsmanager get-secret-value --secret-id MyAnsibleSecretCred --region us-east-1 | jq ".SecretString" | sed 's/\\n //g; s/\ \n//g; s/\\"/"/g; s/"\\n/"/g; s/^"//; s/"$//' | jq ".pub_key" | sed 's/\"//g' >> ~/.ssh/authorized_keys`
+# aws secretsmanager get-secret-value --secret-id MyAnsibleSecretCred --region us-east-1 | jq ".SecretString" | sed 's/\\n //g; s/\ \n//g; s/\\"/"/g; s/"\\n/"/g; s/^"//; s/"$//' | jq ".pub_key" | sed 's/\"//g' >> ~/.ssh/authorized_keys
