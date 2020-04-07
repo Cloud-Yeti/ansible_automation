4dbc6b36ed7a1c48a1562c2f8d8e4a6b6769b497
Robin Gautam <38055190+rbngtm1@users.noreply.github.com>
2020-04-07 18:58:23 -0500

    Update install_ansible_localhost.sh

--- scripts/install_ansible_localhost.sh
+++ scripts/install_ansible_localhost.sh
@@ -3,7 +3,9 @@ sudo yum update -y
 sudo yum install -y python-boto python-boto3
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo amazon-linux-extras install ansible2 -y
-sudo yum install ansible -y
+sudo yum install -y git python python-devel python-pip openssl ansible
+yum install ansible -y
+sudo pip install boto
 sudo bash -c "cat /etc/ansible/ansible.cfg | sed 's/basic default values.../basic default values...\ninventory      = \/etc\/ansible\/hosts/g' | sed 's/basic default values...\nsudo_user     = root/g' > /etc/ansible/ansible.cfg.replacement"
 sudo cp /etc/ansible/ansible.cfg.replacement /etc/ansible/ansible.cfg
 sudo mv /etc/ansible/hosts /etc/hosts.original
