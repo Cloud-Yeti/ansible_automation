c93058f55b992fb0e7484ac0b8d789c01022c432
Robin Gautam <38055190+rbngtm1@users.noreply.github.com>
2020-04-12 16:13:07 -0500

    Update install_ansible_localhost.sh

--- scripts/install_ansible_localhost.sh
+++ scripts/install_ansible_localhost.sh
@@ -6,10 +6,6 @@ sudo amazon-linux-extras install ansible2 -y
 sudo yum install -y git python python-devel python-pip openssl ansible
 yum install ansible -y
 sudo pip install boto
-sudo mv /etc/ansible/ansible.cfg /etc/ansible/ansible.backup
-sudo bash -c "cat /etc/ansible/ansible.cfg | sed 's/basic default values.../basic default values...\ninventory      = \/etc\/ansible\/hosts/g' | sed 's/basic default values...\nsudo_user     = root/g' > /etc/ansible/ansible.cfg.replacement"
-sudo cp /etc/ansible/ansible.cfg.replacement /etc/ansible/ansible.cfg
-sudo mv /etc/ansible/hosts /etc/hosts.original
 sudo bash -c 'echo "[local]" > /etc/ansible/hosts' && sudo bash -c 'echo "localhost ansible_connection=local" >> /etc/ansible/hosts'
 sudo useradd ansible
 #echo 'ansible' | sudo -S passwd ansible # Note the password of ansible user here is ansible
